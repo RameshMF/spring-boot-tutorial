0e55c79f30e46506d9de0a71aff4e60408bfa990
Ramesh Fadatare <rfadatare@avaya.com>
2019-06-14 12:54:49 +0530

    Added Spring boot WebSocket example

---
+++ springboot-stomp-websocket/src/main/java/net/javaguides/springboot/websocket/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package net.javaguides.springboot.websocket.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import net.javaguides.springboot.websocket.model.Greeting;
+import net.javaguides.springboot.websocket.model.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+}
---
+++ springboot-stomp-websocket/src/main/java/net/javaguides/springboot/websocket/model/Greeting.java
@@ -0,0 +1,18 @@
+package net.javaguides.springboot.websocket.model;
+
+public class Greeting {
+
+    private String content;
+
+    public Greeting() {
+    }
+
+    public Greeting(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
