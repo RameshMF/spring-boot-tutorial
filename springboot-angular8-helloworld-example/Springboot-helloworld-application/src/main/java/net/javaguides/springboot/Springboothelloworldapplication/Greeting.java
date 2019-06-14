01d67cc1bbe15def55f69117eebc57a741c4d443
Ramesh Fadatare <rfadatare@avaya.com>
2019-06-14 12:43:17 +0530

    Added spring boot + Angular 8 example tutorial

---
+++ springboot-angular8-helloworld-example/Springboot-helloworld-application/src/main/java/net/javaguides/springboot/Springboothelloworldapplication/Greeting.java
@@ -0,0 +1,19 @@
+package net.javaguides.springboot.Springboothelloworldapplication;
+
+public class Greeting {
+	private final long id;
+	private final String content;
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
