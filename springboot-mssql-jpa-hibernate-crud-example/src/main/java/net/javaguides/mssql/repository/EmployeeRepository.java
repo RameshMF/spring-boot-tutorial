7e4800f0c87526f8d75c1ca01aa0bc92ea0c5900
Ramesh Fadatare <rameshfadatare@Rameshs-MacBook-Air.local>
2023-03-29 14:31:16 +0530

    Upgraded to Spring Boot 3 and Java 17

---
+++ springboot-mssql-jpa-hibernate-crud-example/src/main/java/net/javaguides/mssql/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package net.guides.springboot2.crud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import net.guides.springboot2.crud.model.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
