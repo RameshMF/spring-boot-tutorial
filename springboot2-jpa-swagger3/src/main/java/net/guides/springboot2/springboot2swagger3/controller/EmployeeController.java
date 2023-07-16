74a467a64cf51f61bd0de4209d333b357608da98
Raja Kolli <rajadilipkolli@gmail.com>
2023-07-16 22:32:14 +0000

    Upgrade to spring boot 3

--- springboot2-jpa-swagger3/src/main/java/net/guides/springboot2/springboot2swagger3/controller/EmployeeController.java
+++ springboot2-jpa-swagger3/src/main/java/net/guides/springboot2/springboot2swagger3/controller/EmployeeController.java
@@ -4,7 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
--- springboot2-jpa-swagger3/src/main/java/net/guides/springboot2/springboot2swagger3/model/Employee.java
+++ springboot2-jpa-swagger3/src/main/java/net/guides/springboot2/springboot2swagger3/model/Employee.java
@@ -2,12 +2,12 @@ package net.guides.springboot2.springboot2swagger3.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "employees")
